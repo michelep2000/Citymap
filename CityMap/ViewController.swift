6975f23643b8f3d4f5f40f1c633d0fc63b8cd143
Michele Alfonso Pardo Pezzullo <michele.pardo@utmac0003.local>
2019-12-02 15:47:45 +0100

    Initial Commit

---
+++ CityMap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CityMap
+//
+//  Created by Michele Alfonso Pardo Pezzullo on 2/12/19.
+//  Copyright © 2019 MICHELE ALFONSO PARDO PEZZULLO. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
